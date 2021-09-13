86928ec29e7935d9e5cf51d03a2f347e9a33f7d7
Ernando Rezende <ernando.03@gmail.com>
2021-09-13 01:49:09 -0300

    Datatable Integration

--- frontend/src/types/Sale.ts
+++ frontend/src/types/Sale.ts
@@ -1,3 +1,25 @@
+import { Seller } from './Seller';
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElemens?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
